d850b93194eac9ee4d58d440e2759c8b5128a1f1
Gopika <gopikak75317@gmail.com>
2026-07-10 15:31:28 +0530

    array pr 4 smallest

--- conditions.js
+++ conditions.js
@@ -1,44 +1,8 @@
-// let nums = [12,45,89,67,34,32.51];
-
-// let highest = nums[0];
-
-// for(i=1;i<nums.length;i++){
-//     if(nums[i]>highest){
-//         highest = nums[i];
-//     }
-
-// }
-
-// console.log("Highest Number",highest);d
-
-let num = [56,78,29,21,10,9,54,41,101,765];
-let highest = num[0];
-for(i=1;i<num.length;i++){
-    if(num[i]>highest){
-        highest = num[i];
+let num = [20,10,-200,9000];
+let small = num[0];
+for(let i=1;i<num.length;i++){
+    if(num[i]<small){
+        small = num[i];
     }
 }
-console.log("Highest Number is ",highest);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(small);
