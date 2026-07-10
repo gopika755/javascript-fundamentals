335df7d4ee237855a5e729c395406d0077d42f35
Gopika <gopikak75317@gmail.com>
2026-07-10 15:07:30 +0530

    array pr 3 highest

--- conditions.js
+++ conditions.js
@@ -1,4 +1,44 @@
-let iseven = (num) =>{
-    return num % 2 == 0;
-};
-console.log(iseven(12))
+// let nums = [12,45,89,67,34,32.51];
+
+// let highest = nums[0];
+
+// for(i=1;i<nums.length;i++){
+//     if(nums[i]>highest){
+//         highest = nums[i];
+//     }
+
+// }
+
+// console.log("Highest Number",highest);d
+
+let num = [56,78,29,21,10,9,54,41,101,765];
+let highest = num[0];
+for(i=1;i<num.length;i++){
+    if(num[i]>highest){
+        highest = num[i];
+    }
+}
+console.log("Highest Number is ",highest);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
