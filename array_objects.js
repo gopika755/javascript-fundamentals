9a73e74c05fe013afc6a00c0ab558db7f1f3294c
Gopika <gopikak75317@gmail.com>
2026-07-16 15:28:12 +0530

    Filter Expensive Products

--- array_objects.js
+++ array_objects.js
@@ -1,8 +1,8 @@
-let employee ={
-    name:"gopika",
-    address:{
-        city:"kannur",
-        state:"kerala",
-    }
-};
-console.log(employee.address.city);
+let cosmetics =[
+    {name:"Lipstic",price:10000},
+    {name:"Foundation",price:23000},
+    {name:"Primer",price:12000},
+    {name:"Concealer",price:34000}
+];
+let expensive = cosmetics.filter(cosmetics => cosmetics.price > 11000);
+console.log(expensive);
