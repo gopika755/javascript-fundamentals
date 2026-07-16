42788cf8ec86b88b5051ccb25ae237bb2370b324
Gopika <gopikak75317@gmail.com>
2026-07-16 13:15:54 +0530

    Print Every User

--- array_objects.js
+++ array_objects.js
@@ -1,2 +1,12 @@
-let fruits = ["Apple","Orange","Banana","Kiwi"];
-console.log(fruits.includes("Kiwi"));
+let users =[
+    {name:"GOPIKA",age:18,isactive:true},
+    {name:"NAJLA",age:18,isactive:false},
+    {name:"ANUSHA",age:19,isactive:true},
+    {name:"SAFA",age:19,isactive:false},
+    {name:"NAJIYA",age:18,isactive:false},
+    {name:"FIDHA",age:18,isactive:true},
+];
+users.forEach(users =>{
+    console.log(users.name);
+}
+)
