1a03ef5a19e9f2d19bb2df9f97b58e5385992880
Gopika <gopikak75317@gmail.com>
2026-07-16 13:09:56 +0530

    Check if an Item Exists

--- array_objects.js
+++ array_objects.js
@@ -1,10 +1,2 @@
-let users =[
-    {name:"GOPIKA",age:18,isactive:true},
-    {name:"NAJLA",age:18,isactive:false},
-    {name:"ANUSHA",age:19,isactive:true},
-    {name:"SAFA",age:19,isactive:false},
-    {name:"NAJIYA",age:18,isactive:false},
-    {name:"FIDHA",age:18,isactive:true},
-];
-let user =users.find(users => users.name === "NAJLA");
-console.log(user);
+let fruits = ["Apple","Orange","Banana","Kiwi"];
+console.log(fruits.includes("Kiwi"));
