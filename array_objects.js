2718a88be0184ff13ba7936cb67e6caabdcb818d
Gopika <gopikak75317@gmail.com>
2026-07-16 12:54:22 +0530

    Array of User Objects

---
+++ array_objects.js
@@ -0,0 +1,9 @@
+let users =[
+    {name:"GOPIKA",age:18,isactive:true},
+    {name:"NAJLA",age:18,isactive:false},
+    {name:"ANUSHA",age:19,isactive:true},
+    {name:"SAFA",age:19,isactive:false},
+    {name:"NAJIYA",age:18,isactive:false},
+    {name:"FIDHA",age:18,isactive:true},
+];
+console.log(users);
