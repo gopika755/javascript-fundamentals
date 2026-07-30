5a7a2d974de5b4425269edc1f8b2bd902473d4a5
Gopika <gopikak75317@gmail.com>
2026-07-30 12:43:21 +0530

    Object Destructuring

---
+++ destructuring.js
@@ -0,0 +1,39 @@
+const students ={
+    name:"gopika",
+    age:"18",
+    course:"python full-stack "
+};
+const{name,age,course} = students;
+
+console.log(name);
+console.log(age);
+console.log(course);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
