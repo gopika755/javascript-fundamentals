6e6c2a19f457204dcf397f767387dfb3881f16a4
Gopika <gopikak75317@gmail.com>
2026-07-30 12:52:43 +0530

    Array Destructuring

--- destructuring.js
+++ destructuring.js
@@ -1,39 +1,5 @@
-const students ={
-    name:"gopika",
-    age:"18",
-    course:"python full-stack "
-};
-const{name,age,course} = students;
-
-console.log(name);
-console.log(age);
-console.log(course);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const colors =["Red","Green","yellow"];
+const[first,second,third] =colors;
+console.log(first);
+console.log(second);
+console.log(third);
