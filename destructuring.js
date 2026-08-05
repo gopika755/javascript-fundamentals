6ba2bea14cb45c8a3f6edbd24d8af2335413d472
Gopika <gopikak75317@gmail.com>
2026-08-05 15:46:03 +0530

    Spread Operator

--- destructuring.js
+++ destructuring.js
@@ -1,9 +1,9 @@
-const product ={
-    title:"Phone",
-    price:25000,
-    brand:"Samsung"
-};
-const {title,price,brand}=product;
+const student = {
+    name:"Gopika",
+    age:18,
 
-console.log(title);
-console.log(price);
+};
+const clone ={
+    ...student
+};
+console.log(clone);
