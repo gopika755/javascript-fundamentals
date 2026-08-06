09d96fae081d4b749f3461b0d45817655a6c18f7
Gopika <gopikak75317@gmail.com>
2026-08-06 12:41:11 +0530

    Create the variables

---
+++ practice.js
@@ -0,0 +1,9 @@
+let name = "Gopika";
+let age = 18;
+let isStudent = true;
+let favoriteLanguage = "English";
+
+console.log("Name: ",name);
+console.log("Age: ",age);
+console.log("Student: ",isStudent);
+console.log("FavoriteLanguage: ",favoriteLanguage);
