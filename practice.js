b17a6263206ed64d14e2b70838bdd94f2fa3310a
Gopika <gopikak75317@gmail.com>
2026-08-06 12:50:20 +0530

    Create two numbers and do calculations

--- practice.js
+++ practice.js
@@ -1,9 +1,8 @@
-let name = "Gopika";
-let age = 18;
-let isStudent = true;
-let favoriteLanguage = "English";
+let num1 =10;
+let num2 =15;
 
-console.log("Name: ",name);
-console.log("Age: ",age);
-console.log("Student: ",isStudent);
-console.log("FavoriteLanguage: ",favoriteLanguage);
+console.log("Additiona: ",num1 + num2);
+console.log("Substraction: ",num1 - num2);
+console.log("Multiplication: ",num1 * num2);
+console.log("Division: ",num1 / num2);
+console.log("Modulas: ",num1 % num2);
