2792f42bd02e76bebc2eb346c3f9b6695f1276f6
Gopika <gopikak75317@gmail.com>
2026-08-10 14:46:19 +0530

    Simple Interest

--- practice.js
+++ practice.js
@@ -1,3 +1,7 @@
-let number = 8;
-let square = number **2;
-console.log("Square: ",square);
+let principal = 5000;
+let rate =5;
+let time =2;
+let simpleInterest =(principal * rate * time) / 100;
+let total = principal + simpleInterest;
+console.log("simple Interest: ",simpleInterest);
+console.log("Total amount: ",total);
