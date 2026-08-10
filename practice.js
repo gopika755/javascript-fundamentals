106e1eada044348c9aca7fa20f82b8f23ea758bb
Gopika <gopikak75317@gmail.com>
2026-08-10 10:33:42 +0530

    Swap two variables without using a third variable.

--- practice.js
+++ practice.js
@@ -1,8 +1,7 @@
-let num1 =10;
-let num2 =15;
+let a =10;
+let b =20;
 
-console.log("Additiona: ",num1 + num2);
-console.log("Substraction: ",num1 - num2);
-console.log("Multiplication: ",num1 * num2);
-console.log("Division: ",num1 / num2);
-console.log("Modulas: ",num1 % num2);
+[a, b] = [b,a];
+
+console.log("a =",a);
+console.log("b =",b);
