5f15014f36e7f4fa9578a2cfec66783c46122b10
Gopika <gopikak75317@gmail.com>
2026-08-10 14:43:11 +0530

    Square of a Number

--- practice.js
+++ practice.js
@@ -1,6 +1,3 @@
-let length = 15;
-let width = 10;
-
-let perimeter = 2 * (length + width);
-
-console.log("Perimeter ",perimeter)
+let number = 8;
+let square = number **2;
+console.log("Square: ",square);
