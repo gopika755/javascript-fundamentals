acddcd06b99600fa40c199bbe06c448f4e46a81e
Gopika <gopikak75317@gmail.com>
2026-08-10 10:57:26 +0530

    Add product price total calculation

--- practice.js
+++ practice.js
@@ -1,6 +1,7 @@
-let mark1 = 70;
-let mark2 = 80;
-let mark3 = 90;
+let laptopprice =50000;
+let mouseprice = 1000;
+let keyboardprice = 2000;
 
-let average = (mark1 + mark2 + mark3 )/ 3;
-console.log("Average =",average);
+let total = laptopprice + mouseprice + keyboardprice ;
+
+console.log(total);
