2c799265d0256073a026ac5c9c5042b0406ca430
Gopika <gopikak75317@gmail.com>
2026-08-10 10:48:11 +0530

    This uses a template literal.

--- practice.js
+++ practice.js
@@ -1,7 +1,7 @@
-const country ="India";
+let name = "Gopika";
+let age = 18;
+let city = "Kannur";
 
-console.log(country);
-
-country="Usa";
-
-console.log(country);
+console.log(`Hi my name is ${name}.`);
+console.log(`I'm ${age} years old.`);
+console.log(`I live in ${city}`);
