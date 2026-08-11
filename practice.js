0a00adb542109297a729fcd4cd9549a432c4b128
Gopika <gopikak75317@gmail.com>
2026-08-11 11:29:25 +0530

    Round Numbers

--- practice.js
+++ practice.js
@@ -1,3 +1,5 @@
-let text = "  Hello world   ";
+let number = 8.67;
 
-console.log(text.trim());
+console.log("Round: ", Math.round(number));
+console.log("Floor: ",Math.floor(number));
+console.log("Ceil: ",Math.ceil(number));
