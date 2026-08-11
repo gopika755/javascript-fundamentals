ca0e02a4f3477aa80d07c3634f49cb202d2803ea
Gopika <gopikak75317@gmail.com>
2026-08-11 11:54:14 +0530

    Mini Calculator

--- practice.js
+++ practice.js
@@ -1,4 +1,9 @@
-let number = 15.6789;
+let num1 =15;
+let num2 =3;
 
-console.log(number.toFixed(1));
-console.log(number.toFixed(2));
+console.log("Addition: ",num1 + num2);
+console.log("Substraction: ",num1 - num2);
+console.log("Division: ",num1 /  num2);
+console.log("Multiplication: ",num1 * num2);
+console.log("Modulas: ",num1 % num2);
+console.log("Exponant: ",num1 ** num2);
