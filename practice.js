c8d7738712849f8014515c576b8e60a3c2dcaa23
Gopika <gopikak75317@gmail.com>
2026-08-11 14:29:30 +0530

    Month name switch

--- practice.js
+++ practice.js
@@ -1,20 +1,40 @@
-let a = 20;
-let b = 5;
-let operator = "+";
+let month =8;
 
-switch(operator){
-    case "+":
-        console.log(a+b);
+switch(month){
+    case 1:
+        console.log("January");
         break;
-    case "*":
-        console.log(a*b);
+    case 2:
+        console.log("February");
         break;
-    case "-":
-        console.log(a-b);
+    case 3:
+        console.log("March");
         break;
-    case "/":
-        console.log(a/b);
+    case 4:
+        console.log("April");
         break;
-    default:
-        console.log("Invalid operator");
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
 };
