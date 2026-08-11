e1f03d96d8b466a4e8d56f4f88f0e5cbcf457a15
Gopika <gopikak75317@gmail.com>
2026-08-11 12:41:27 +0530

    Temperature Check

--- practice.js
+++ practice.js
@@ -1,13 +1,9 @@
-let mark = 85;
+let temparature = -5;
 
-if(mark >= 90){
-    console.log("Grade A");
-}else if(mark >=80){
-    console.log("Grade B");
-}else if(mark > 70){
-    console.log("Grade C");
-}else if(mark > 60){
-    console.log("Grade D")
+if(temparature > 30){
+    console.log("HOT");
+}else if(temparature >=20){
+    console.log("NORMAL");
 }else{
-    console.log("Grade F");
-};
+    console.log("COLD")
+}
