0ce5509758ac7bef9df30401df0103180bd71631
Gopika <gopikak75317@gmail.com>
2026-08-11 12:27:59 +0530

    Find Greater Number

--- practice.js
+++ practice.js
@@ -1,7 +1,9 @@
-let age = 10;
 
-if(age > 18){
-    console.log("Eligible");
-}else {
-    console.log("Not Eligible");
+let a =45;
+let b =7;
+
+if(a>b){
+    console.log("Greater: ",a);
+}else{
+    console.log("Greater: ",b);
 };
