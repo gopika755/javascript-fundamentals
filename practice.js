197b151b047f445708b1bcc10b3d4f76746c56fc
Gopika <gopikak75317@gmail.com>
2026-08-11 15:28:47 +0530

    Create a Function

--- practice.js
+++ practice.js
@@ -1,8 +1,4 @@
-for(let i=1;i<=10;i++){
-    let stars ="";
-
-    for(let j=1;j<=i;j++){
-        stars += "*";
-    };
-    console.log(stars);
-};
+function greet(){
+    console.log("Hello");
+}
+greet()
