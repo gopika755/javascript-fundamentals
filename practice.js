479fe9269417816ba9208664c33bdece0b7fe383
Gopika <gopikak75317@gmail.com>
2026-08-11 12:18:22 +0530

    Positive, Negative or Zero using if

--- practice.js
+++ practice.js
@@ -1,9 +1,9 @@
-let num1 =15;
-let num2 =3;
+let number = -6
 
-console.log("Addition: ",num1 + num2);
-console.log("Substraction: ",num1 - num2);
-console.log("Division: ",num1 /  num2);
-console.log("Multiplication: ",num1 * num2);
-console.log("Modulas: ",num1 % num2);
-console.log("Exponant: ",num1 ** num2);
+if(number >0){
+    console.log("Positive");
+}else if(number <0 ){
+    console.log("Negative");
+}else{
+    console.log("zero");
+};
