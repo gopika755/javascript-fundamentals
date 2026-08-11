b7d4647d0a6ede214cd7ee4406d9792c2033f9f7
Gopika <gopikak75317@gmail.com>
2026-08-11 15:22:55 +0530

    Print a Star Pattern

--- practice.js
+++ practice.js
@@ -1,7 +1,8 @@
-let i =1;
-let sum=0;
-while(i <= 10){
-    sum=sum+i;
-    i++;
+for(let i=1;i<=10;i++){
+    let stars ="";
+
+    for(let j=1;j<=i;j++){
+        stars += "*";
+    };
+    console.log(stars);
 };
-console.log("SUM: ",sum)
