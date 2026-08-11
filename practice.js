846eb7a11824ac35e0b5b7a0540fb65c152b1603
Gopika <gopikak75317@gmail.com>
2026-08-11 14:12:30 +0530

    Day Name using switch

--- practice.js
+++ practice.js
@@ -1,7 +1,25 @@
-let age = 55 
-let isStudent = true;
-if(age >= 60 || isStudent === true){
-    console.log("Discount available");
-}else{
-    console.log("NO discount");
+let day = 5;
+
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break
 };
