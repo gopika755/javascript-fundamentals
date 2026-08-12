1b41e2ea217f00dabe3b3786cf8c0cc7b27a3e1b
Gopika <gopikak75317@gmail.com>
2026-08-12 12:37:43 +0530

    Check Leap Year

--- practice.js
+++ practice.js
@@ -1,10 +1,11 @@
-function findlargest(a,b,c){
-    if(a>b && a>c){
-        return a;
-    }else if(b>a && b>c){
-        return b; 
-    }else{
-        return c;
-    };
+let year = 2024;
+
+if(year % 400 === 0){
+    console.log("Leap year");
+}else if(year % 100 ===0){
+    console.log("Not a Leap Year");
+}else if(year % 4 === 0){
+    console.log("Leap year");
+}else{
+    console.log("Not a Leap year");
 };
-console.log(findlargest(100,180,70));
