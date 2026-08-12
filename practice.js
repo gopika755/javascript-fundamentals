4bfdac461f0d20ed5a2ffd02637fc47a4105c6b6
Gopika <gopikak75317@gmail.com>
2026-08-12 14:20:48 +0530

    Check Palindrome Number

--- practice.js
+++ practice.js
@@ -1,12 +1,14 @@
-let number =54321;
-
-let reverse =0;
-
-while(number >0){
+let number = 1231;
+let orginal= number;
+let reverse=0;
+while(number>0){
     let digit = number % 10;
-    reverse = reverse * 10 + digit;
 
-    number =Math.floor(number/10);
-
-}
-console.log("Reverse: ",reverse);
+    reverse = reverse * 10 +digit;
+    number = Math.floor(number/10);
+};
+    if (orginal === reverse){
+        console.log("Plindrome");
+    }else{
+        console.log("Not palindrome");
+    }
