fc989c8f3911fd93cea4c66e62f122959a18c770
Gopika <gopikak75317@gmail.com>
2026-08-12 12:28:19 +0530

    Find Highest of Three Numbers

--- practice.js
+++ practice.js
@@ -1,3 +1,10 @@
-const greet = name => `Hello ${name}`;
-console.log(greet("Gopika"));
-
+function findlargest(a,b,c){
+    if(a>b && a>c){
+        return a;
+    }else if(b>a && b>c){
+        return b; 
+    }else{
+        return c;
+    };
+};
+console.log(findlargest(100,180,70));
