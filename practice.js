ff70398cb139e11e5f7ecd4b1fdac2a3e9bd435d
Gopika <gopikak75317@gmail.com>
2026-08-12 14:15:49 +0530

    Reverse a Number

--- practice.js
+++ practice.js
@@ -1,11 +1,12 @@
-let year = 2024;
+let number =54321;
 
-if(year % 400 === 0){
-    console.log("Leap year");
-}else if(year % 100 ===0){
-    console.log("Not a Leap Year");
-}else if(year % 4 === 0){
-    console.log("Leap year");
-}else{
-    console.log("Not a Leap year");
-};
+let reverse =0;
+
+while(number >0){
+    let digit = number % 10;
+    reverse = reverse * 10 + digit;
+
+    number =Math.floor(number/10);
+
+}
+console.log("Reverse: ",reverse);
