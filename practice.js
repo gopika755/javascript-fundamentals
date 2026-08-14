3ed3ad9c4e457419fd60fba4876dcadc440996c3
Gopika <gopikak75317@gmail.com>
2026-08-14 11:45:01 +0530

    Funtion + loop problem

--- practice.js
+++ practice.js
@@ -1,19 +1,13 @@
-let number= 17;
-let isPrime = true;
+for(let number = 2;number <=50;number ++){
+    let isPrime = true;
 
-if(number <= 1){
-    isPrime = false;
-}
+    for(let i=2;i<number;i++){
+        if(number % i == 0){
+            isPrime = false;
+        }
+    }
 
-for(i=2;i<number;i++){
-    if(number % i ==0){
-        isPrime = false;
-        break;
+    if(isPrime){
+        console.log(number);
     }
 }
-
-if(isPrime){
-    console.log("Prime");
-}else{
-    console.log("Not Prime");
-};
