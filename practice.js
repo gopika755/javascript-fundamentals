472b8c7584d7cc4f65c3058326b5e7970c8dd917
Gopika <gopikak75317@gmail.com>
2026-08-14 12:21:07 +0530

    function to sum of digits

--- practice.js
+++ practice.js
@@ -1,9 +1,11 @@
-let number = 12345;
-sum = 0;
+function sumofdigit(number){
+    let sum =0
 
-while(number>0){
-    let digit = number % 10;
-    sum +=digit;
-    number =Math.floor(number / 10);
+    while(number>0){
+        let digit = number % 10;
+        sum += digit
+        number =Math.floor(number / 10);
+    }
+    return sum;
 }
-console.log(sum);
+console.log(sumofdigit(123456));
