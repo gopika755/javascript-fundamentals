bb4c153e85c7c34b80923159d235a003951f1c72
Gopika <gopikak75317@gmail.com>
2026-08-17 11:50:19 +0530

    Number Guess Logic

--- practice.js
+++ practice.js
@@ -1,15 +1,9 @@
-let mark1 = 80;
-let mark2 = 75;
-let mark3 = 90;
-
-let total = mark1 + mark2 + mark3 ;
-let average = total / 3;
-
-console.log("Total:",total);
-console.log("Average:",average);
-
-if(average > 70){
-    console.log("Pass");
+let secretnumber = 7;
+let guess = 15;
+if(guess===secretnumber){
+    console.log("correct");
+}else if(guess > secretnumber){
+    console.log("Too High");
 }else{
-    console.log("Fail");
+    console.log("Too Low");
 };
